f9a6a54c92ee5057bfb12574661a139a0c903fd0
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2022-10-30 16:59:27 -0400

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import { ThemeProvider } from "next-themes";
+import Script from 'next/script';
 import "../css/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-3PPL9VQKC1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3PPL9VQKC1');
+        `}
+      </Script>
       <Component {...pageProps} />
     </ThemeProvider>
   );
